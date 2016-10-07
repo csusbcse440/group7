ef45c1a501713fdc23431481c761c87f90c7defa
Edwin Baranov <edwin.baranov@gmail.com>
2016-10-06 22:39:26 -0700

    Add files via upload

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+
+    public Transform lookAt;
+    public Transform camTransform;
+    public float distance = 50.0f;
+    public float yHeight = 80.0f;
+    public float sensitivityX = 5.5f;
+    private Camera cam;
+
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+
+    private void Start() {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(1)) {
+            currentX += Input.GetAxis("Mouse X") * sensitivityX;
+        }
+        currentY = yHeight;
+    }
+
+    private void LateUpdate() {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    public float moveSpeed = 20.0f;
+
+    private Rigidbody rb;
+    private Camera playerCamera;
+    private Vector3 keyboardInput;
+    private Vector3 playerDirection;
+    private Vector3 moveVelocity;
+    private int cameraLayermask;
+
+    // Use this for initialization //
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+        playerCamera = Camera.main;
+
+        // Layers for Camera Raycast to focus on //
+        int groundLayer = 8;
+
+        // Add layers to Layermask
+        cameraLayermask = 1 << groundLayer;
+    }
+
+    // Update is called once per frame //
+    void Update() {
+
+        keyboardInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        playerDirection = playerCamera.transform.TransformDirection(keyboardInput);
+        playerDirection.y = 0f;
+        playerDirection.Normalize();
+        print(playerDirection);
+
+        moveVelocity = playerDirection * moveSpeed;
+
+        if (!Input.GetMouseButton(1)) {
+
+            Ray cameraRay = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(cameraRay, out hit, Mathf.Infinity, cameraLayermask)) {
+
+                Debug.DrawLine(cameraRay.origin, hit.point, Color.blue);
+
+                transform.LookAt(new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z));
+            }
+        }
+    }
+
+    void FixedUpdate() {
+
+        //Replace Gravity
+        moveVelocity.y = rb.velocity.y;
+        rb.velocity = moveVelocity;
+
+    }
+}
